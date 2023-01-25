e5fcfe44cc000a908da03439d6cbabc991bf3b32
William Henrique <william.martins@ee.ufcg.edu.br>
2023-01-25 16:15:50 -0300

    azure secret keys

---
+++ src/IIoT-Projeto/lib/app/azure-key.h
@@ -0,0 +1,7 @@
+
+/** @brief IOT HUB NAME */ 
+#define IOTHUB_NAME     ""
+/** @brief IOT HUB Device ID */ 
+#define IOTHUB_DEVID    ""
+/** @brief IOT HUB Device SaS Key */ 
+#define IOTHUB_KEY      ""
