24e15e4a03ea0cacc086d5db4859f03377ab1823
William Henrique <william.martins@ee.ufcg.edu.br>
2023-02-13 14:58:34 -0300

    removido teste de derivativa de chaves

--- src/IIoT-Projeto/lib/app/mqtt.h
+++ src/IIoT-Projeto/lib/app/mqtt.h
@@ -9,16 +9,7 @@
 #include "common.h"
 #include "fft.h"
 
-#ifndef NOAZUREKEYS
 #include "azure-key.h"
-#else
-/** @brief IOT HUB NAME */ 
-#define IOTHUB_NAME     "iothub-name"
-/** @brief IOT HUB Device ID */ 
-#define IOTHUB_DEVID    "dev-id"
-/** @brief IOT HUB Device SaS Key */ 
-#define IOTHUB_KEY      "sas-token"
-#endif
 
 #define W2P "devices/"IOTHUB_DEVID"/messages/events/"
 
